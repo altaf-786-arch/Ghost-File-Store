2521fde94b65c60cc6be92c971ac0199ad4940d7
altaf-786-arch <find4funny@gmail.com>
2025-08-07 14:33:16 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -17,7 +17,7 @@ import re
 import json
 import base64
 from urllib.parse import quote_plus
-from TechVJ.utils.file_properties import get_name, get_hash, get_media_file_size
+from Ghost.utils.file_properties import get_name, get_hash, get_media_file_size
 logger = logging.getLogger(__name__)
 
 BATCH_FILES = {}
