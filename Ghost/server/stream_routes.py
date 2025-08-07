6446c84ec805654f54224b7352734033a8a9a737
altaf-786-arch <find4funny@gmail.com>
2025-08-07 14:24:09 +0530

    Update stream_routes.py

--- Ghost/server/stream_routes.py
+++ Ghost/server/stream_routes.py
@@ -6,12 +6,12 @@ import secrets
 import mimetypes
 from aiohttp import web
 from aiohttp.http_exceptions import BadStatusLine
-from TechVJ.bot import multi_clients, work_loads, StreamBot
-from TechVJ.server.exceptions import FIleNotFound, InvalidHash
-from TechVJ import StartTime, __version__
+from Ghost.bot import multi_clients, work_loads, StreamBot
+from Ghost.server.exceptions import FIleNotFound, InvalidHash
+from Ghost import StartTime, __version__
 from ..utils.time_format import get_readable_time
 from ..utils.custom_dl import ByteStreamer
-from TechVJ.utils.render_template import render_page
+from Ghost.utils.render_template import render_page
 from config import MULTI_CLIENT
 
 
